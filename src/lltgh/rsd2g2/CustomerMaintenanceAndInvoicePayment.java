cd728059d71f823b902c1c8f7fd287fe95ee78d0
Timothy Goh <timothy.gcy@outlook.com>
2018-12-22 23:44:33 +0800

    mod cust

--- src/lltgh/rsd2g2/CustomerMaintenanceAndInvoicePayment.java
+++ src/lltgh/rsd2g2/CustomerMaintenanceAndInvoicePayment.java
@@ -11,8 +11,6 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-//import Customized.ListInterface;
-//import Customized.List;
 
 public class CustomerMaintenanceAndInvoicePayment {
 
--- src/lltgh/rsd2g2/InvListInterface.java
+++ src/lltgh/rsd2g2/InvListInterface.java
@@ -4,6 +4,7 @@ public interface InvListInterface<T> {
     public void add(T item);
     //boolean add(int index, T item);
     public boolean contains(T item);
+    public boolean update(int index, T newEntry);
     public T remove(int index);
     public int size();
     public boolean isEmpty();
